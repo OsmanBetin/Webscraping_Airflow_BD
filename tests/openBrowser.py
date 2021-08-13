50cd28312b59c2fbb97cbe08bd24aa4e609f11f8
OsmanFernandez <osman.fernandez@outlook.com>
2021-08-12 22:05:07 -0500

    Prueba_Inicial

---
+++ tests/openBrowser.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/home/osmanfernandez/ProjectsBal/Webscraping_Airflow_BD/SeleniumDriver/chromedriver')
+
+driver.get('https://www.python.org')
